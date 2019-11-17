8ec4accbecb19b574330857c9acda51704f23fab
Lennon Puype <lennonpuype@hotmail.com>
2019-11-17 13:57:56 +0100

    Remove Boxes

--- development/swift_dev/ARPictionary/ARPictionary/ViewController.swift
+++ development/swift_dev/ARPictionary/ARPictionary/ViewController.swift
@@ -14,8 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var arView: ARSCNView!
     
     @IBAction func Button(_ sender: Any) {
-        print("Button Pressed");
+        print("Plus Pressed");
         addBox();
+    }
+    
+    @IBAction func MinButton(_ sender: Any) {
+        print("Min Pressed");
+        removeAllBoxes();
     }
     
     let configuration = ARWorldTrackingConfiguration()
@@ -40,21 +45,22 @@ class ViewController: UIViewController {
     }
     
     func addBox(){
-        
-        let randomX = Float.random() * (0 - 0.5) + 0;
-        let randomY = Float.random() * (0 - 0.5) + 0;
-        let randomZ = Float.random() * (0 - 0.5) + 0;
-        
-        print(randomZ)
-        
         let boxNode = SCNNode()
         boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.002)
-        //boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
         boxNode.geometry?.firstMaterial?.diffuse.contents = UIImageView(image: #imageLiteral(resourceName: "Material"))
-        boxNode.position = SCNVector3(randomX,randomY,randomZ)//x,y,z
+        boxNode.position = SCNVector3(Float.random(-0.5, max: 0.5) , Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5))//x,y,z
         
+        boxNode.name = "box";
         //Add to the scene
         arView.scene.rootNode.addChildNode(boxNode);
+    }
+    
+    func removeAllBoxes(){
+        arView.scene.rootNode.enumerateChildNodes{(node, _) in
+            if (node.name == "box"){
+                node.removeFromParentNode()
+            }
+        }
     }
     
     override var prefersStatusBarHidden: Bool{
