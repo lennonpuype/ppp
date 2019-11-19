8b91ba404d17dd73e140036621d9446276ba8b51
Lennon Puype <lennonpuype@hotmail.com>
2019-11-19 19:29:49 +0100

    First c# application - Hello World

---
+++ development/c#-tests/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿//Import
+using System;
+
+//Name from file & export name
+namespace HelloWorld
+{
+    class MainClass
+    {
+        //Init function
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
