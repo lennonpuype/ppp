937222694f63755930238db56b93115194f37ad0
Lennon Puype <lennonpuype@hotmail.com>
2019-11-24 13:26:29 +0100

    nothing major

--- development/unity-app-test/unity-app-test/Assets/NameTransfer.cs
+++ development/unity-app-test/unity-app-test/Assets/NameTransfer.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
 
 public class NameTransfer : MonoBehaviour
 {
@@ -13,5 +16,26 @@ public class NameTransfer : MonoBehaviour
 	{
 		theName = inputField.GetComponent<Text>().text;
 		textDisplay.GetComponent<Text>().text = "Welcome " + theName + " to the game";
-	}
+
+
+        
+        Console.WriteLine(Dns.GetHostEntry("localhost").AddressList[0]);
+        //try
+        //{
+        //    //Socket Listener
+        //    IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+        //    TcpListener server = new TcpListener(ip, 8080);
+        //    TcpClient client = default(TcpClient);
+
+        //    server.Start();
+        //    Console.WriteLine("Server Started");
+        //    Console.Read();
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine(e.ToString());
+        //    Console.Read();
+        //}
+
+    }
 }
---
+++ development/unity-app-test/unity-app-test/Assets/PhotonConnect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonConnect : MonoBehaviour
+{
+    public GameObject sectionView1, sectionView2, sectionView3;
+
+    public static void connectToPhoton()
+    {
+        PhotonNetwork.ConnectUsingSettings(true);
+        Debug.Log("Connecting to photon...");
+    }
+
+    private static void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("We are connected to master");
+    }
+
+    private void OnJoinedLobby()
+    {
+        sectionView1.SetActive(false);
+        sectionView2.SetActive(true);
+        Debug.Log("On Joined Lobby");
+    }
+
+    private void OnDisconnectedFromPhoton()
+    {
+        if (sectionView1.active)
+        {
+            sectionView1.SetActive(false);
+        }
+
+        if (sectionView2.active)
+        {
+            sectionView2.SetActive(false);
+        }
+
+        sectionView3.SetActive(true);
+
+        Debug.Log("On Joined Lobby");
+    }
+
+    private void onFailedToConnectToPhoton()
+    {
+        Debug.Log("Disconnect from photon services");
+    }
+
+}
